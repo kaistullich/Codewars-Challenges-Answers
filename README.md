82855cc90b75865686b94b56f6b8ab907dfb8ae4
kaistullich <kairstullich@gmail.com>
2017-08-28 22:53:47 -0700

    finished middle character

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Codewars Challenges
 
-This is a repository with my answers to [Codewars Challenges](https://www.codewars.com/) 
+This is a repository with my answers to [Codewars Challenges](https://www.codewars.com/).
 These answers may or may not be the most optimal, but they are my way of solving these problems.
 
 I hope to further improve my coding abilities with these challenges, and to come up with new
--- get-middle-character.py
+++ get-middle-character.py
@@ -1,10 +1,35 @@
-def get_middle(s):
-    wordLength = len(s)
+import math
 
+
+def get_middle(s):
+    """
+    For any given string grab the middle character. If the length of the word is ODD,
+    grab the middle character. If the length of the word is EVEN grab the 2 middle
+    characters.
+
+    :param s: word [str]
+    :return: middle character(s) [str]
+    """
+
+    wordLength = len(s)
+    # create list with 1 character at a time
+    splitWord = list(s)
+    # if its an even word
     if wordLength % 2 == 0:
-        print('Its an even word')
+        evenChars = ''
+        # the left character in the middle
+        leftIndex = int((wordLength / 2) - 1)
+        # the right character in the middle
+        rightIndex = int(((wordLength / 2) + 1) - 1)
+        # add both characters to new string
+        evenChars += (splitWord[leftIndex])
+        evenChars += splitWord[rightIndex]
+        return evenChars
+    # if its an odd word
     else:
-        print('odd length word')
+        # grab middle character
+        oddIndex = int(math.ceil(wordLength / 2) - 1)
+        return splitWord[oddIndex]
 
 if __name__ == '__main__':
     get_middle('test')
