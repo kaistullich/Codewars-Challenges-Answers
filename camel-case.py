6f726b308e9bf4d80552a4a8628908299a88fa68
kaistullich <kairstullich@gmail.com>
2017-08-30 18:15:53 -0700

    added comments to camel-case.py and test library

--- camel-case.py
+++ camel-case.py
@@ -6,6 +6,18 @@ def to_camel_case(text):
     Complete the method/function so that it converts dash/underscore delimited words into camel casing.
     The first word within the output should be capitalized only if the original word was capitalized.
 
+    Examples:
+
+        to_camel_case("the-stealth-warrior") # returns "theStealthWarrior"
+
+        to_camel_case("The_Stealth_Warrior") # returns "TheStealthWarrior"
+
+        to_camel_case('') # returns ''
+
+        to_camel_case('A-B-C') # returns ABC
+
+        to_camel_case('The-pippi-was_Hungry') # returns ThePippiWasHungry
+
     :param text: a word [str]
     :return: camel case of the word [str]
     """
@@ -27,6 +39,19 @@ def to_camel_case(text):
         return camelWordFirstWord
 
 
+def test_to_camel_case():
+    """
+    install `pytest` library if not already installed:
+        - pip install -U pytest
+
+    For testing run `pytest <filename>.py`
+    """
+    assert to_camel_case('the-stealth-warrior') == 'theStealthWarrior'
+    assert to_camel_case("The_Stealth_Warrior") == 'TheStealthWarrior'
+    assert to_camel_case('') == ''
+    assert to_camel_case('A-B-C') == 'Abc'
+    assert to_camel_case('The-pippi-was_Hungry') == 'ThePippiWasHungry'
+
 if __name__ == '__main__':
     # returns "theStealthWarrior"
     stealth = to_camel_case("the-stealth-warrior")
@@ -37,7 +62,7 @@ if __name__ == '__main__':
     print(stealth1)
 
     # returns ''
-    none = ('')
+    none = to_camel_case('')
     print(none)
 
     # returns ABC
